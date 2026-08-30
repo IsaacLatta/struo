3bb3e3cde3d65ae5014d03c348dfe8577aa6653a
Isaac <isaaclatta73@gmail.com>
2026-08-29 18:09:59 -0700

    wrote sanity check test on linked libs

--- tests/dummy_test.cpp
+++ tests/dummy_test.cpp
@@ -1,5 +1,10 @@
 #include <gtest/gtest.h>
+#include "struo/struo.hpp"
 
 TEST(DummyTests, Suceeds) {
+    EXPECT_EQ(struo::get_project_version(), std::string_view{"0.1"});
+    EXPECT_TRUE(struo::is_debug_enabled());
+    EXPECT_TRUE(struo::is_yaml_linked());
+    EXPECT_EQ(struo::get_enum_name(struo::FileFormat::YAML), std::string_view{"YAML"});
     SUCCEED();
 }
